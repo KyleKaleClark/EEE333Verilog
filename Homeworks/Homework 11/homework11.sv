ce76addcfa603fda304a943d386a4773236de365
Kyle_Clark <kjclark1998@gmail.com>
2022-06-28 13:58:32 -0700

    Update homework11.sv

--- Homeworks/Homework 11/homework11.sv
+++ Homeworks/Homework 11/homework11.sv
@@ -29,3 +29,45 @@ module average5point(input clk, reset, input [Width-1:0] X, input REG Coef, outp
 	end
 	assign Y = YY;
 endmodule
+
+
+
+
+`timescale 1ns/1ps
+module filterf_tb();
+	localparam N = 20;
+	REG Coef;
+	integer i, fd, fr, fc, err;
+	logic clk, reset;
+	logic [Width-1:0] X, Y;
+	
+	average5point F0 (clk, reset, X, Coef, Y);
+	
+	initial begin
+		fr = $fopen("xinput.csv", "r");
+		fc = $fopen("coef.csv", "r");
+		fd = $fopen("Filterma.csv");
+		$fwrite(fd, "#, X, Y\n");
+		for(i=0; i<Size; i=i+1)
+			err = $fscanf(fc, "%d", Coef.Z[i]);
+		$fclose(fc);
+		
+		X = 0; clk = 1'b0; reset = 1'b1; #10;
+		reset = 1'b0; #10;
+		
+		for(i=0; i<Size; i=i+1) begin
+			clk = 1'b1; #10;
+			clk = 1'b0; #10;
+		end		
+		
+		for(i=0;i<N;i=i+1) begin
+			err = $fscanf(fr, "%d", X);
+			clk = 1'b1; #10;
+			clk = 1'b0; #10;
+			$display("X=%d, Y=%d", X, Y/5);
+			$fwrite(fd, "%d, %d, %d\n", i, X, Y/5);
+		end		
+		$fclose(fr);
+		$fclose(fd);
+	end
+endmodule
